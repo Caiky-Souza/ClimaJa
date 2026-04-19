d7c3f830ebe5a16b5b18e03f29aa59ad7a7bd3b6
Caiky-Souza <caikyhortasouza25@gmail.com>
2026-04-18 21:03:45 -0300

    Redesenho e correção

--- script.js
+++ script.js
@@ -1,41 +1,120 @@
-async function fetchGeoLocation(local){
-    let {results} = await (await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${local}&count=10&language=en&format=json`)).json()
-    let new_results = []
+let locationsList = [];
+const results = document.querySelector(".results")
+let searchInput = document.querySelector(".search-input")
+let resultText = document.querySelector(".result-text");
 
+function getFlagEmoji(countryCode) {
+const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt(0));
+return String.fromCodePoint(...codePoints);
+};
+
+function updateData(button){
+    let locName = document.querySelector(".local-name")
+    locName.textContent = button.srcElement.childNodes[1].innerHTML
+    let latitude = button.srcElement.childNodes[2].innerHTML
+    let longitude = button.srcElement.childNodes[3].innerHTML
+
+    let info = getLocationInfo(latitude,longitude)
+    //await getLocationInfo()
+    //let data = 
+    //let name =  
+};
+async function getLocationInfo(latitude, longitude){
+    let promise = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=True`)
+    let response = await promise.json()
+    console.log(response)
+};
+
+
+
+async function getLocationsList(local){
+    let {results} = await (await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${local}&count=5&language=en&format=json`)).json()
+    let newResultsHtml = ""
+    let newResultsList = []
     results.forEach(element => {
-        if (new_results.length < 5){
-            new_results.push(element.name)
-        };
+        newResultsHtml += (`<p class="result-item"><span class="location-name">${element.name}</span> - ${getFlagEmoji(element.country_code)} <span class="hidden latitude">${element.latitude}</span><span class="hidden longitude">${element.longitude}</span></p>`)
+        newResultsList.push(element)
         
     });
-    return new_results
+
+    if (searchInput.value){
+        locationsList = newResultsList
+        return newResultsHtml
+    };
+
+    return ""
 }
-
-async function typedInInput(result){
-    let div = document.querySelector(".result-text"); 
-    div.innerHTML = `${result}`
-
-
-}
-
-async function chgLetter(event){
-    event.preventDefault()
-    local = event.target.value
+async function addSearchList(){
     
-    if (local == ""){
-        typedInInput("")
+    results.classList.forEach((el)=>{
+        if (el == "hidden") {
+        results.classList.remove("hidden")
+        }
+
+    })
+    
+    let result = await getLocationsList(searchInput.value)
+    updateList(result)
+    
+
+};
+
+// Remover a lista de locais
+
+function rmvSearchList(){
+    resultText.textContent = ''
+    results.classList.add("hidden")
+};
+
+
+// Função para atualizar a lista de locais
+function updateList(result){
+    // Atualiza o container
+    resultText.innerHTML = `${result}`;
+    document.querySelector(".result-item").addEventListener("click",updateData)
+};
+
+// Atualiza a lista de locais
+async function updateLocationsList(event){
+    event.preventDefault()
+    input = event.target.value
+
+    // Se não há nada escrito
+    if (input == ""){
+
+        // Remove a lista de resultados
+        rmvSearchList();
+
     }
+    // Se tiver algo escrito
     else {
-        result = await fetchGeoLocation(local)
-        typedInInput(result)
+        //  Adiciona a lista de resultados
+        addSearchList();
+        
+        // Pega uma lista de locais baseados no input
+        result = await getLocationsList(input);
+        
+        // Atualiza o resultado com os locais adquiridos
+        updateList(result);
     }
     
 };
 
+// Função principal
 function main(){
-    document.querySelector(".search-input").addEventListener("input",chgLetter)
 
+    // Quando alguém digita algo
+    document.querySelector(".search-input").addEventListener("input",updateLocationsList)
+    
+    // Quando alguém foca no input
+    document.querySelector(".search-container").addEventListener("focusin",addSearchList)
+    // Quando alguém desfoca do input
+    
 
+    
 };
 
 main();
